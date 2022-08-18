6bb3a5569e7f1574be47d0fb913d72e9b33a8a21
BrundaPuttaraju <108655391+BrundaPuttaraju@users.noreply.github.com>
2022-08-18 15:30:01 +0530

    commit new inheritance practice code

---
+++ Inheritance/src/example/singleInheritance/com/SingleInheritance.java
@@ -0,0 +1,10 @@
+package example.singleInheritance.com;
+
+public class SingleInheritance {
+
+	public static void main(String[] args) {
+		Car c = new Car("Maruti Suzuki", 1996, "Suzuki Alto" , "Red");
+		c.carDetails();
+	}
+
+}
