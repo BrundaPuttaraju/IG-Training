8611bf0f70c859901f6a71f589d37c2f5cd5efa8
BrundaPuttaraju <108655391+BrundaPuttaraju@users.noreply.github.com>
2022-09-12 10:52:44 +0530

    practice crud operations

---
+++ spring-boot-crud-operation/src/main/java/com/demo/crud/repository/BooksRepository.java
@@ -0,0 +1,7 @@
+package com.demo.crud.repository;
+import org.springframework.data.repository.CrudRepository;
+
+import com.demo.crud.model.Books;
+public interface BooksRepository extends CrudRepository<Books, Integer>
+{
+}
---
+++ spring-boot-crud-operation/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Books (bookid,bookname,author,price) VALUES (1, 'JAVA','James Gosling','100');
+INSERT INTO Books (bookid,bookname,author,price) VALUES (2, 'ADVANCED JAVA','James Gosling','200');
+INSERT INTO Books (bookid,bookname,author,price) VALUES (3, 'SPRINGBOOT','Rod Johnson','300');
+INSERT INTO Books (bookid,bookname,author,price) VALUES (4, 'MYSQL','Michael','400');
