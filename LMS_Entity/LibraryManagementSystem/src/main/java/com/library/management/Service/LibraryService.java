f7890f26b3c0a96146e0d8d756c44fe0987d9423
BrundaPuttaraju <108655391+BrundaPuttaraju@users.noreply.github.com>
2022-09-14 14:19:56 +0530

    commit service code

---
+++ LMS_Entity/LibraryManagementSystem/src/main/java/com/library/management/Service/LibraryService.java
@@ -0,0 +1,27 @@
+package com.library.management.Service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.library.management.Entity.User;
+import com.library.management.enumtype.UserType;
+
+public interface LibraryService {
+
+	@Service
+	public interface UserService {
+		
+		public User saveUser(User user);
+
+		public List<User> getAllUser();
+
+		public User getUserById(long id);
+
+		public List<User> getUserByName(String name);
+
+		public User updateUser(User user, long id);
+
+		public void deleteUser(long id);
+
+		public List<UserType> getuserByType(String type);
+	}
+}
